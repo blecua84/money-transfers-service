cb1d7a879a3627fdb6262fcf3bd64116e7c12bcf
Jose Javier Blecua de Pedro <josejavier.blecua@gmail.com>
2019-06-05 08:39:10 +0100

    refactor(AccountDAO): Remove modifiers.

--- src/main/java/com/blecua84/moneytransfers/persistence/daos/AccountDAO.java
+++ src/main/java/com/blecua84/moneytransfers/persistence/daos/AccountDAO.java
@@ -5,16 +5,16 @@ import com.blecua84.moneytransfers.services.models.Account;
 
 public interface AccountDAO {
 
-    public String GET_ACCOUNT_BY_SORT_CODE_AND_NUMBER_QUERY = "from Account where sortCode=:SC and accountNumber=:AN";
-    public String ACCOUNT_NULL_MESSAGE = "Input account is null or empty";
-    public String ACCOUNT_CANNOT_BE_UPDATED = "Input account cannot be updated";
-    public String ACCOUNT_CANNOT_BE_SAVED = "There was an error trying to save the account";
-    public String ACCOUNT_CANNOT_BE_FETCHED = "There was an error trying to get the account";
-    public String ACCOUNT_DOES_NOT_EXIST = "Account does not exist.";
+    String GET_ACCOUNT_BY_SORT_CODE_AND_NUMBER_QUERY = "from Account where sortCode=:SC and accountNumber=:AN";
+    String ACCOUNT_NULL_MESSAGE = "Input account is null or empty";
+    String ACCOUNT_CANNOT_BE_UPDATED = "Input account cannot be updated";
+    String ACCOUNT_CANNOT_BE_SAVED = "There was an error trying to save the account";
+    String ACCOUNT_CANNOT_BE_FETCHED = "There was an error trying to get the account";
+    String ACCOUNT_DOES_NOT_EXIST = "Account does not exist.";
 
-    public void saveAccount(Account account) throws DataManagerException;
+    void saveAccount(Account account) throws DataManagerException;
 
-    public Account getAccountBySortCodeAndNumber(String sortCode, String accountNumber) throws DataManagerException;
+    Account getAccountBySortCodeAndNumber(String sortCode, String accountNumber) throws DataManagerException;
 
-    public void updateAccount(Account account) throws DataManagerException;
+    void updateAccount(Account account) throws DataManagerException;
 }
